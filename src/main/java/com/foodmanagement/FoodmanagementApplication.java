29569b7c507e5da916bf888eeeca5bb9e9f0c740
marco-oliveira <marco-oliveira@live.com>
2016-08-10 09:45:40 -0300

    commit com a estrutura do projeto, e suas principais dependências

---
+++ src/main/java/com/foodmanagement/FoodmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.foodmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodmanagementApplication.class, args);
+	}
+}
